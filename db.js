e8bf3069abad5579808bbce62607d47b73d9d34e
RajivKhattri <np03cs4a230274@heraldcollege.edu.np>
2025-07-16 13:41:27 +0545

    Second successful hosting attempt

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const mongoURL = process.env.MONGODB_URL;
 mongoose.connect(mongoURL,{
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useUnifiedTopology: true,
     ssl: true
 })
 
